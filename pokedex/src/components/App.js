929a7aca1e42dc3f9fe6c6f45f220358ebf3b801
leo011619 <leonard.ferraz@hotmail.com>
2021-03-29 18:29:16 -0300

    Feature: criação do detail view

--- pokedex/src/components/App.js
+++ pokedex/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import DetailView from './DetailView';
 import PokeList from './PokeList'
 import './styles/App.css'
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
     return (
       <div className = "App">
       <PokeList />
+      <DetailView />
       </div>
     );
   }
